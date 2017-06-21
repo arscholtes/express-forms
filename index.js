e92dd39b4f9a9f1f435b10a722f4910f0649c604
Edwin Marshall <edwin.marshall@theironyard.com>
2017-06-21 08:43:04 -0400

    Cleaned up some code.

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+/* module imports *************************************************************/
 const express = require("express");
 const bodyParser = require("body-parser");
-console.log(bodyParser);
 
 const app = express();
 
+/* middleware *****************************************************************/
+// serve static content out of this directory
 app.use(express.static("./"));
+// parse all requests using the generic body parser (req.body is now available)
 app.use(bodyParser());
 
-app.post("/", (req, res) => {
-    const email = req.body.email;
-    res.send(
-        "<p>Your user name is: " + email + "</p>"
-    );
+/* routes *********************************************************************/
+app.post("/", function(req, res) {
+    // get the email from the request body
+    res.send("<p>Your user name is: " + req.body.email + "</p>");
 });
 
+// start
 app.listen(8000, () => console.log("ready!"));
